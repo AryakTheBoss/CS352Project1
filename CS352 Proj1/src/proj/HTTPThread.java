8174629a6fdda71799514052f35aec3dd2a085db
JakeTc <tcjake863@gmail.com>
2020-09-30 00:22:23 -0400

    Did most of the parsing and error checking (haven't tested yet).

--- CS352 Proj1/src/proj/HTTPThread.java
+++ CS352 Proj1/src/proj/HTTPThread.java
@@ -1,6 +1,9 @@
 package proj;
 
 import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class HTTPThread extends Thread {
 
@@ -19,10 +22,163 @@ public class HTTPThread extends Thread {
 
 
     @Override
-    public void run() {
+    public void run(){
         super.run(); //PLACEHOLDER
+        
+        String request = "";//holds the initial request line
+        String temp; //used for reading in additional lines
+        String restOfRequest; //holds any lines after the initial request line
+        
+        //get the HTTP request
+        //initial line is stored in request
+        //additional lines are stored in restOfRequest
+        try {
+        	BufferedReader inFromServer = new BufferedReader(new
+        				InputStreamReader(client.getInputStream()));
+        	
+        	request = inFromServer.readLine(); //initial line
+        	temp = inFromServer.readLine(); //line after
+        	restOfRequest = ""; //will store everything after the initial line
+        	
+        	//get the rest of the response
+        	while(!(temp.isEmpty())) {
+        		restOfRequest = restOfRequest + temp + "\n";
+        	}
+        	
+        	//hopefully returning from a thread is allowed
+        } catch (IOException ioe) {
+        	System.out.println("File Read Error");
+        	return;
+        }
+        
+        //parse HTTP request
+        //splits up the initial line, checks it for errors, and then hands over the arguments to other methods
+        String[] initialLine = request.split(" |\t");
+        
+        if (!initialLineErrorChecking(initialLine)) {
+        	System.out.println("Initial Line Error");
+        	return;
+        }
+        
+        //Calls the particular command specified by the first token
+        if(initialLine[0].equals("GET")) {
+        	get(initialLine);
+        } else if(initialLine[0].equals("POST")) {
+        	post(initialLine);
+        } else if(initialLine[0].equals("HEAD")) {
+        	head(initialLine);
+        } else if(initialLine[0].equals("DELETE")) {
+        	delete(initialLine);
+        } else if(initialLine[0].equals("PUT")) {
+        	put(initialLine);
+        } else if(initialLine[0].equals("LINK")) {
+        	link(initialLine);
+        } else {
+        	unlink(initialLine);
+        }
     }
-
+    
+    /**
+     * 
+     * @param initialLine	holds the arguments in the initial line
+     * @return true if no errors were encountered, false otherwise
+     */
+    private boolean initialLineErrorChecking(String[] initialLine) {
+    	
+    	if(initialLine.length != 3) {
+    		return false;
+    	}
+    	
+    	//checks if the first string is equal to any of these things
+    	if(!(initialLine[0].equals("GET")
+    			|| initialLine[0].equals("POST")
+    			|| initialLine[0].equals("HEAD")
+    			|| initialLine[0].equals("DELETE")
+    			|| initialLine[0].equals("PUT")
+    			|| initialLine[0].equals("LINK")
+    			|| initialLine[0].equals("UNLINK"))) {
+    		return false;
+    	}
+    	
+    	//checks if the last string has HTTP/ in the front
+    	//not sure if this will work
+    	if(!(initialLine[2].matches("HTTP/(.*)"))) {
+    		return false;
+    	}
+    	
+    	//check if the last string has a number after the HTTP/
+    	int endOfVersionNumber = initialLine[2].length(); //gets the length of the third token of the command line
+    	String versionNumber = initialLine[2].substring(5, endOfVersionNumber); //gets the substring that is supposed to have the version number
+    	
+    	//try catch statement will check if the "version number" is a double
+    	try {
+    		double version = Double.parseDouble(versionNumber);
+    	} catch (Exception e) {
+    		return false;
+    	}
+    	
+    	//statement to check if the path is a vaild path?
+    	//NEED TO DO THIS LATER
+    	
+    	return true;
+    }
+    
+    /**
+     * Will implement the GET method of HTTP protocol
+     * @param initialLine
+     */
+    public void get(String[] initialLine) {
+    	
+    }
+    
+    /**
+     * Will implement the POST method of HTTP protocol
+     * @param initialLine
+     */
+    public void post(String[] initialLine) {
+    	
+    }
+    
+    /**
+     * Will implement the HEAD method of HTTP protocol
+     * @param initialLine
+     */
+    public void head(String[] initialLine) {
+    	
+    }
+    
+    /**
+     * Will implement the DELETE method of HTTP protocol
+     * @param initialLine
+     */
+    public void delete(String[] initialLine) {
+    	
+    }
+    
+    /**
+     * Will implement the PUT method of HTTP protocol
+     * @param initialLine
+     */
+    public void put(String[] initialLine) {
+    	
+    }
+    
+    /**
+     * Will implement the LINK method of HTTP protocol
+     * @param initialLine
+     */
+    public void link(String[] initialLine) {
+    	
+    }
+    
+    /**
+     * Will implement the UNLINK method of HTTP protocol
+     * @param initialLine
+     */
+    public void unlink(String[] initialLine) {
+    	
+    }
+    
     @Override
     public void interrupt() {
         super.interrupt(); //PLACEHOLDER
