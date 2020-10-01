cd357be06c5052d42635cdd73bcbdb2c4a216eca
sena <senakm98@gmail.com>
2020-10-01 02:51:14 -0400

    httpthread.java

--- CS352 Proj1/src/proj/HTTPThread.java
+++ CS352 Proj1/src/proj/HTTPThread.java
@@ -2,10 +2,16 @@ package proj;
 
 import java.io.*;
 import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.File;
+import java.io.DataOutputStream
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Scanner;
+import java.net.URLConnection
 
 public class HTTPThread extends Thread {
 
@@ -99,14 +105,12 @@ public class HTTPThread extends Thread {
     			|| initialLine[0].equals("PUT")
     			|| initialLine[0].equals("LINK")
     			|| initialLine[0].equals("UNLINK"))) {
-    	    System.err.println("HTTP/1.0 501 Not Implemented");
     		return false;
     	}
     	
     	//checks if the last string has HTTP/ in the front
     	//not sure if this will work
     	if(!(initialLine[2].matches("HTTP/(.*)"))) {
-            System.err.println("HTTP/1.0 400 Bad Request");
     		return false;
     	}
     	
@@ -118,7 +122,6 @@ public class HTTPThread extends Thread {
     	try {
     		double version = Double.parseDouble(versionNumber);
     	} catch (Exception e) {
-            System.err.println("HTTP/1.0 400 Bad Request");
     		return false;
     	}
     	
@@ -236,7 +239,101 @@ public class HTTPThread extends Thread {
      * @param initialLine
      */
     public void head(String[] initialLine) {
-    	
+        Date d = new Date();
+        Calendar c = Calendar.getInstance();
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        File f = new File(initialLine[1]);
+        int fLength = (int) f.length();
+        String header = "HTTP/1.0 200 OK"; //the initial header line
+        String allow="",contentEncoding="",contentLength="",contentType="",expires="",lastModified=""; //head components
+
+        /*possibly (emphasis on possibly) simpler way for below:
+        URLConnection c = f.toURL().openConnection();
+        String contentType = c.getContentType();//just returns a string mime type
+
+        if(contentType.equals("text/html")){
+            allow = "\Allow: GET, HEAD, POST";
+        }else if(contentType.equals("application-zip")){
+            contentEncoding="\nContent-Encoding: zip";
+            allow = "\nAllow: GET, HEAD";
+        }else if(contentType.equals("application/x-gzip")
+            allow = "\nAllow: GET, HEAD";
+            contentEncoding="\nContent-Encoding: x-gzip";
+        else{
+            allow = "\nAllow: GET, HEAD";
+        }
+        */
+
+
+        if(initialLine[1].endsWith("html")||initialLine[1].endsWith("htm")){
+            contentType = "\nContent-Type: text/html";
+            allow = "\nAllow: GET, HEAD, POST"; //not sure if post is allowed on html files
+        }else if(initialLine[1].endsWith("txt")){
+            contentType = "\nContent-Type: text/plain";
+            allow = "\nAllow: GET, HEAD";
+        }else if(initialLine[1].endsWith("gif")){
+            contentType= "\nContent-Type: image/gif";
+            allow = "\nAllow: GET, HEAD";
+        }else if(initialLine[1].endsWith("png")){
+            contentType= "\nContent-Type: image/png";
+            allow = "\nAllow: GET, HEAD";
+        }else if(initialLine[1].endsWith("jpg")){
+            contentType= "\nContent-Type: image/jpeg";
+            allow = "\nAllow: GET, HEAD";
+        }else if(initialLine[1].endsWith("pdf")){
+            contentType= "\nContent-Type: application/pdf";
+            allow = "\nAllow: GET, HEAD";
+        }else if(initialLine[1].endsWith("zip")){
+            contentType= "\nContent-Type: application/zip";
+            contentEncoding="\nContent-Encoding: zip";
+            allow = "\nAllow: GET, HEAD";
+        }else if(initialLine[1].endsWith("gz")){ //idk if that the extension that denotes x-gzip
+            contentType= "\nContent-Type: application/x-gzip";
+            allow = "\nAllow: GET, HEAD";
+            contentEncoding="\nContent-Encoding: x-gzip";
+        }else{
+            contentType = "\nContent Type: application/octet-stream"; //unknown file type
+            allow = "\nAllow: GET, HEAD";
+        }
+
+
+        c.setTime(d);
+        contentLength = "\nContent-Length: "+fLength; //size of file in bytes
+        lastModified = "\nLast-Modified: "+formatter.format(f.lastModified());
+        c.add(Calendar.YEAR, 1);
+        expires = "\nExpires: "+formatter.format(c.getTime());
+        header += allow+contentEncoding+contentLength+contentType+expires+lastModified+"\n";
+
+        if(initialLine[1].endsWith("html")||initialLine[1].endsWith("htm")||initialLine[1].endsWith("txt")) {
+                try {
+                    DataOutputStream os = new DataOutputStream(client.getOutputStream());
+                    os.writeChars(header);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }else{
+
+                try {
+                    DataOutputStream os = new DataOutputStream(client.getOutputStream());
+                    os.writeBytes(header);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+
+
+        try {
+            client.close(); //close the socket
+        } catch (IOException e) {
+            System.err.println("HTTP/1.0 500 Internal Server Error");
+        }
+    }
+
+
     }
     
     /**
