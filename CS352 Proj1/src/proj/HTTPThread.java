0b4b30e86ed189e4766299e2de8ea3a6fcd7e6fe
JakeTc <tcjake863@gmail.com>
2020-10-02 02:20:38 -0400

    Added the majority of the Response status codes (ex. 404).

--- CS352 Proj1/src/proj/HTTPThread.java
+++ CS352 Proj1/src/proj/HTTPThread.java
@@ -10,6 +10,8 @@ import java.io.DataOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
+import java.text.ParseException;
 import java.util.Scanner;
 import java.net.URLConnection;
 
@@ -51,7 +53,16 @@ public class HTTPThread extends Thread {
         	
         	//get the rest of the response
         	while(!(temp.isEmpty())) {
-        		restOfRequest = restOfRequest + temp + "\n";
+        		
+        		//if there is a space or tab in the front, the line belongs to the previous header line
+        		if(temp.charAt(0) == '\t' || temp.charAt(0) == ' ') {
+        			restOfRequest = restOfRequest + temp;
+        		
+        		//else, the line contains a new header line, so make a new line
+        		} else {
+        			restOfRequest = restOfRequest + "\n" + temp;
+        		}
+        		
         	}
         	
         	//hopefully returning from a thread is allowed
@@ -60,12 +71,89 @@ public class HTTPThread extends Thread {
         	return;
         }
         
+        DataOutputStream outToClient = null;
+      //gets a file stream that will send data to the client
+    	try {
+            outToClient = new DataOutputStream(client.getOutputStream());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
         //parse HTTP request
         //splits up the initial line, checks it for errors, and then hands over the arguments to other methods
         String[] initialLine = request.split(" |\t");
         
         if (!initialLineErrorChecking(initialLine)) {
-        	//System.err.println("HTTP/1.0 400 Bad Request");
+        	
+        	System.err.println("HTTP/1.0 400 Bad Request");
+        	
+        	try {
+				outToClient.writeChars("HTTP/1.0 400 Bad Request");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        	return;
+        }
+        
+        //attempt to read the second argument as a file
+    	File file = new File(initialLine[1]);
+    	
+    	//check if the file exists
+    	if(!(file.exists())) {
+    		System.err.println("HTTP/1.0 404 Not Found");
+    		
+    		try {
+				outToClient.writeChars("HTTP/1.0 404 Not Found");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        	return;
+    	}
+    	
+    	//see if the file is forbidden THIS ONLY WORKS ON LINUX!!
+    	if(!(file.canRead())) {
+    		System.err.println("HTTP/1.0 403 Forbidden");
+        	
+        	try {
+				outToClient.writeChars("HTTP/1.0 403 Forbidden");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        	return;
+    	}
+        
+        //checks if the file was modified or not
+        if(checkDate(restOfRequest, file)) {
+        	
+        	System.err.println("HTTP/1.0 204 Not Modified");
+        	
+        	try {
+				outToClient.writeChars("HTTP/1.0 204 Not Modified");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        	return;
+        }
+        
+        //check if the HTTP version is supported
+        int endOfVersionNumber = initialLine[2].length(); //gets the length of the third token of the command line
+    	String versionNumber = initialLine[2].substring(5, endOfVersionNumber); //gets the substring that is supposed to have the version number
+    	
+    	double version = Double.parseDouble(versionNumber);
+        if(version > 1.0) {
+        	System.err.println("HTTP/1.0 505 HTTP Version Not Supported");
+        	
+        	try {
+				outToClient.writeChars("\"HTTP/1.0 505 HTTP Version Not Supported");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
         	return;
         }
         
@@ -86,13 +174,6 @@ public class HTTPThread extends Thread {
         	unlink(initialLine);
         }
         
-        //Close the connection and return (so thread can go back to the threadpool
-        try {
-        	client.close();
-        } catch (IOException ioe) {
-        	System.err.println("Error closing the connection.");
-        }
-        return;
     }
     
     /**
@@ -134,15 +215,58 @@ public class HTTPThread extends Thread {
     		return false;
     	}
     	
-    	//check if the path is a vaild path
-    	File file = new File(initialLine[1]);
+    	return true;
+    }
+    
+    /**
+     * Will check if the file has been modified ever since the specified date
+     * @param ifModified the string that holds the date.
+     * @return	boolean	if the file wasn't modified ever since the if-modified-since header date, returns true, false otherwise
+     */
+    public boolean checkDate(String headers, File file) {
+    	//separates the first header from the rest of the headers
+    	String [] arr = headers.split(" ", 2);
     	
-    	//check if the file exists
-    	if(!(file.exists())) {
-    		return false;
+    	//while the header is not the correct header, go to the next line and repeat.
+    	//if we reach the end, there is no ifmodified header.
+    	while(!(arr[0].equalsIgnoreCase("If-Modified-Since:")) || arr.length == 1) {
+    		arr = arr[1].split("\n", 2); //removes that line, and goes to the next line, and splits that
+    		arr = arr[1].split(" ", 2); //separates the first header from the rest of the headers
     	}
     	
-    	return true;
+    	//if there is no ifModified specification, just return true
+    	if(arr.length == 1) {
+    		return true;
+    	}
+    	
+    	//now arr[1] up to the next line contains the date. Throws away the other headers
+    	arr = arr[1].split("\n", 2);
+    	String date = arr[0];
+    	
+    	//interpret this date
+    	SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.US);
+    	
+    	//supposedly, the parse method is not threadsafe
+    	//lock this line, and check for parsing errors.
+    	//will turn the string into a date.
+    	Date ifModified = null;
+    	synchronized(this) {
+    		try {
+    			ifModified = dateFormat.parse(date);//line that is locked
+    		} catch (ParseException pe) {
+    			System.err.println("Error parsing date");
+    		}
+    	}
+    	
+    	//get the date the file was last modified
+    	Date modified = new Date(file.lastModified());
+    	
+    	//compare the dates, if the ifModified date is before modified date, return true
+    	if ((ifModified.compareTo(modified)) < 0) {
+    		return true;
+    	}
+    	
+    	return false;
     }
     
     /**
