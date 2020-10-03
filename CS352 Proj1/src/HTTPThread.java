29e893d6395b60c8f48516e82858bb36adf2c902
JakeTc <tcjake863@gmail.com>
2020-10-02 22:27:01 -0400

    bugsx3

--- CS352 Proj1/src/HTTPThread.java
+++ CS352 Proj1/src/HTTPThread.java
@@ -57,12 +57,13 @@ public class HTTPThread extends Thread {
 	        	temp = inFromServer.readLine(); //line after
 	        	restOfRequest = ""; //will store everything after the initial line
 	        	
-	        	
+	        	boolean first = true;
 	        	//get the rest of the response
 	        	while(!(temp.isEmpty())) {
 	        		
 	        		//if there is a space or tab in the front, the line belongs to the previous header line
-	        		if(temp.charAt(0) == '\t' || temp.charAt(0) == ' ') {
+	        		if(temp.charAt(0) == '\t' || temp.charAt(0) == ' ' || first) {
+	        			first = false;
 	        			restOfRequest = restOfRequest + temp;
 	        		
 	        		//else, the line contains a new header line, so make a new line
@@ -132,8 +133,7 @@ public class HTTPThread extends Thread {
         
         //checks if the file was modified or not
         if(!checkDate(restOfRequest, file)) {
-        	sendError("304 Not Modified\r\n"
-        			+ "Expires: a future date\r\n", outToClient);
+        	sendError("304 Not Modified", outToClient);
         	return;
         }
         
